c4277de785ece981362e434983b1d05227263b9d
Douaesb <douae.sb411@gmail.com>
2023-11-16 12:00:26 +0100

    EquipeTable

--- connection.php
+++ connection.php
@@ -8,6 +8,6 @@ $conn = mysqli_connect($servername, $username, $password, $database);
 if ($conn->connect_error) {
 die("Connection failed: " . $conn->connect_error);
 }
-mysqli_close($conn);
+// mysqli_close($conn);
 
 ?>
--- index.php
+++ index.php
@@ -1,6 +1,94 @@
 <?php
 include("connection.php");
-
-echo "hello world !";
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>DataWare</title>
+</head>
+
+<body>
+    <div class="bg-gray-100 py-10">
+
+        <div class="px-4 sm:px-6 lg:px-8">
+            <div class="sm:flex sm:items-center">
+                <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+                    <button type="button" class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">Ajouter une Equipe</button>
+                </div>
+            </div>
+            <div class="mt-8 flex flex-col">
+                <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div class="inline-block min-w-full py-2 align-middle">
+                        <div class="overflow-hidden shadow-sm ring-1 ring-black ring-opacity-5">
+                            <table class="min-w-full divide-y divide-gray-300">
+                                <thead class="bg-gray-50">
+                                    <tr>
+                                        <th scope="col" class="py-3.5 pl-4 pr-3 text-center text-sm font-semibold text-gray-900 sm:pl-6 lg:pl-8">Id Equipe</th>
+                                        <th scope="col" class="px-3 py-3.5 text-center text-sm font-semibold text-gray-900">Nom Equipe</th>
+                                        <th scope="col" class="px-3 py-3.5 text-center text-sm font-semibold text-gray-900">Date de Creation</th>
+                                        <th scope="col" class="px-3 py-3.5 text-center text-sm font-semibold text-gray-900">
+                                            <span>Modifier</span>
+                                        </th>
+                                        <th scope="col" class="px-3 py-3.5 text-center text-sm font-semibold text-gray-900">
+                                            <span>Supprimer</span>
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody class="divide-y divide-gray-200 bg-white">
+
+                                    <tr>
+
+                                        <?php
+                                        $sql = "SELECT * FROM equipe";
+                                        $result = $conn->query($sql);
+                                        if ($result->num_rows > 0) {
+                                            while ($row = $result->fetch_assoc()) {
+                                        ?>
+                                                <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm text-center font-medium text-gray-900 sm:pl-6 lg:pl-8">
+                                                    <?php
+                                                    echo $row["id_equipe"];
+                                                    ?>
+                                                </td>
+                                                <td class="whitespace-nowrap px-3 py-4 text-sm text-center text-gray-500">
+                                                    <?php
+                                                    echo $row["nom_equipe"];
+                                                    ?>
+                                                </td>
+                                                <td class="whitespace-nowrap px-3 py-4 text-sm text-center text-gray-500">
+                                                    <?php
+                                                    echo $row["date_creation"];
+                                                    ?>
+                                                </td>
+                                                <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-center text-sm font-medium sm:pr-6 lg:pr-8">
+                                                    <a href="#" class="text-indigo-600 hover:text-indigo-900">Modifier<span class="sr-only"></span></a>
+                                                </td>
+                                                <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-center text-sm font-medium sm:pr-6 lg:pr-8">
+                                                    <a href="#" class="text-indigo-600 hover:text-indigo-900">Supprimer<span class="sr-only"></span></a>
+                                                </td>
+                                    </tr>
+                            <?php
+                                            }
+                                        } else {
+                                            echo "0 results";
+                                        }
+
+                                        $conn->close();
+                            ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</body>
+
+</html>
